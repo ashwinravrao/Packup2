b1f992be87657d79001d14b442b0f7caa1c7e3fd
Ashwin Rao <ashwinravrao@gmail.com>
2025-08-19 12:25:14 -0500

    remove redundant call to disregard window insets

--- app/src/main/java/com/ashwinrao/packup/ui/MainActivity.kt
+++ app/src/main/java/com/ashwinrao/packup/ui/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
 import com.ashwinrao.packup.PackupApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent { PackupApp() }
     }
