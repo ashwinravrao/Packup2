02d67c8194e7c5b6f3031fb230f563631ad22ffb
Ashwin Rao <ashwinravrao@gmail.com>
2025-08-22 12:23:52 -0500

    add :core:common, create helper function as workaround to broken previews with vm

---
+++ core/common/src/main/java/com/ashwinrao/packup/core/common/composable/PreviewHelpers.kt
@@ -0,0 +1,19 @@
+package com.ashwinrao.packup.core.common.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalInspectionMode
+
+/**
+ * Checks whether the calling composable is "inspectable", aka a `@Preview`. If it is, return the
+ * fake viewmodel implementation of T. Otherwise invoke a function so that the real implementation
+ * can be instantiated via hilt. This is a workaround to a known issue - if a preview calls a composable
+ * containing a viewmodel, the preview will not render.
+ */
+@Composable
+fun <T> getViewModelForInspectionMode(
+    fake: T,
+    real: @Composable () -> T,
+): T {
+    return if (LocalInspectionMode.current) fake
+    else real()
+}
