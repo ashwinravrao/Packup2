77603a6a4fec3f3ba563d322035ba5c378f339c1
Ashwin Rao <ashwinravrao@gmail.com>
2025-08-23 14:50:25 -0500

    encapsulate permissions logic

--- feature/camera/src/main/java/com/ashwinrao/packup/feature/camera/screen/CameraScreen.kt
+++ feature/camera/src/main/java/com/ashwinrao/packup/feature/camera/screen/CameraScreen.kt
@@ -9,9 +9,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
@@ -19,17 +21,22 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.ashwinrao.packup.feature.common.theme.PackupTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
 
-@Composable
 @OptIn(ExperimentalPermissionsApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
 fun CameraScreen(
     modifier: Modifier = Modifier,
     onComplete: () -> Unit,
 ) {
-    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
+    val cameraPermissionState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.CAMERA,
+        )
+    )
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraController: LifecycleCameraController =
@@ -40,7 +47,7 @@ fun CameraScreen(
     }
 
     when {
-        cameraPermissionState.status.isGranted -> {
+        cameraPermissionState.allPermissionsGranted -> {
             Scaffold(
                 modifier = modifier.fillMaxSize()
             ) { innerPadding ->
@@ -55,7 +62,7 @@ fun CameraScreen(
             }
         }
 
-        cameraPermissionState.status.shouldShowRationale -> {
+        cameraPermissionState.shouldShowRationale -> {
             // todo: add a more elaborate message with a placeholder and button/instructions
             //  to show the user how to enable permissions
             Text("Camera permission is needed to use this feature.")
@@ -64,7 +71,39 @@ fun CameraScreen(
         else -> {
             Text("Requesting camera permission...")  // todo: determine if necessary
             LaunchedEffect(Unit) {
-                cameraPermissionState.launchPermissionRequest()
+                cameraPermissionState.launchMultiplePermissionRequest()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun HandleSinglePermissionRequest(
+    requiredPermission: String,
+    onRequested: @Composable () -> Unit,
+    onGranted: @Composable () -> Unit,
+    onTemporarilyDenied: @Composable () -> Unit,
+    onPermanentlyDenied: @Composable () -> Unit,
+) {
+    val state = rememberPermissionState(requiredPermission)
+    val hasRequestedBefore = rememberSaveable { mutableStateOf(false) }
+
+    val isGranted = state.status.isGranted
+    val isTempDenied = state.status.shouldShowRationale
+    val isPermaDenied = hasRequestedBefore.value && !isGranted && !isTempDenied
+
+    when {
+        isGranted -> onGranted()
+        isPermaDenied -> onPermanentlyDenied()
+        isTempDenied -> onTemporarilyDenied()
+        else -> {
+            onRequested() // first time only
+            LaunchedEffect(Unit) {
+                if (!hasRequestedBefore.value) {
+                    hasRequestedBefore.value = true
+                    state.launchPermissionRequest()
+                }
             }
         }
     }
