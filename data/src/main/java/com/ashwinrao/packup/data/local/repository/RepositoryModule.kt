588473aca0ca577c3012691f544eb54e3f6160e0
Ashwin Rao <ashwinravrao@gmail.com>
2025-08-30 15:31:19 -0500

    add binds for repository

---
+++ data/src/main/java/com/ashwinrao/packup/data/local/repository/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.ashwinrao.packup.data.local.repository
+
+import com.ashwinrao.packup.domain.repository.ItemRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindItemRepository(
+        impl: LocalItemRepository
+    ): ItemRepository
+}
