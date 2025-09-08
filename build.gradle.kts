c3fb569e542062ec47a537a9ec3c1b26ec54e6a4
Ashwin Rao <ashwinravrao@gmail.com>
2025-09-07 20:35:44 -0500

    set up ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(libs.ktlint.plugin)
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -6,4 +17,20 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        android.set(true)
+        verbose.set(true)
+
+//        filter {
+//            exclude { element ->
+//                element.file.path.contains("[directory of files to exclude here]")
+//            }
+//        }
+    }
 }
