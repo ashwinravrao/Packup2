31729cb28384c8c81ecfde20c845b5c64c690dcc
Ashwin Rao <ashwinravrao@gmail.com>
2025-09-08 01:20:48 -0500

    revert file copyright headers

--- data/src/main/java/com/ashwinrao/packup/data/local/model/ItemEntity.kt
+++ data/src/main/java/com/ashwinrao/packup/data/local/model/ItemEntity.kt
@@ -1,4 +1,6 @@
-// Copyright (c) 2025 Ashwin R. Rao (github.com/ashwinravrao). All rights reserved.
+/*
+ * Copyright (c) 2025 Ashwin R. Rao (github.com/ashwinravrao). All rights reserved.
+ */
 
 package com.ashwinrao.packup.data.local.model
 
